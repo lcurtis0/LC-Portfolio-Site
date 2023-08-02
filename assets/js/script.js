00e3f64e535c8f0561cfb776762203e439e0267a
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-08-02 00:41:07 -0400

    need to add images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,46 @@
 
 const newText = document.getElementById("newText");
+
+const addArea = document.getElementById("addArea");
+
+const newText2 = document.getElementById("newText2");
+const newText3 = document.getElementById("newText3");
+
 const section =  document.getElementById("section");
 const section2 =  document.getElementById("section2");
 const section3 =  document.getElementById("section3");
 const section4 =  document.getElementById("section4");
 
 const portfolioTitle = document.getElementById("Portfolio");
+const Resume = document.getElementById("Resume");
+const Contact = document.getElementById("Contact");
+const About = document.getElementById("About");
+
 const imgSandDollar = document.getElementById("SandDollar");
+const shellOne = document.getElementById("shell-1");
+
+/*-------------------------------------------------*/
+
 
 section.addEventListener("mouseenter", mouseEnter);
 section.addEventListener("mouseleave", mouseLeave);
 
 function mouseEnter() {
-    newText.textContent = "Hello this is a paragraph";
+    newText.textContent = "Each project you see will progressively implements more web development skill sets.";
     imgSandDollar.style.width = "200px";
     portfolioTitle.style.fontSize= "220%";
+    newText.style.fontSize = "20px";
+    newText.style.opacity = "1";
+
+    addArea.innerHTML = "<a><id=""img src=./assets/images/sanddollar.png></a>"
   }
 
   function mouseLeave() {
     portfolioTitle.style.fontSize = "150%";
     imgSandDollar.style.width = "150px";
     newText.textContent = "";
+    newText.style.fontSize = "2px";
+    newText.style.opacity = "0";
   }
 /*-------------------------------------------------*/
 
@@ -28,12 +48,16 @@ function mouseEnter() {
 document.getElementById("section2").addEventListener("mouseleave", mouseLeave2);
 
 function mouseEnter2() {
-    document.getElementById("section2").style.color = "blue";
+    newText2.textContent = "Hello this is a paragraph Hello this is a paragraph Hello this is a paragraph Hello this is a paragraph";
+    shellOne.style.width = "200px";
+    Resume.style.fontSize= "220%";
 
   }
 
   function mouseLeave2() {
-    document.getElementById("section2").style.color = "black";
+    Resume.style.fontSize = "150%";
+    shellOne.style.width = "150px";
+    newText2.textContent = "";
 
   }
 
